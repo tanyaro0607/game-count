b7647c0996633dfb0466ff274618724962c237a7
tanyaro0607 <ta.rohlova@yandex.ru>
2022-02-15 16:52:59 +0300

    реализация таймера

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const $start = document.querySelector('#start') //кнопка 'начало'
 const $game = document.querySelector('#game') //поле для игры
+const $time = document.querySelector('#time') //счетчик времени
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
 let score = 0
+let isGameStarted = false
 
 //функция начало игры
 function startGame() {
+    isGameStarted = true
     $game.style.backgroundColor = '#fff' //смена цвета на белый
     $start.classList.add('hide') //скрыть кнопку
+
+    let interval = setInterval(function() {
+        let time = parseFloat($time.textContent)
+        if (time <= 0) {
+            clearInterval(interval)
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox() //создать квадрат
 }
 
+function endGame() {
+    isGameStarted = false
+}
+
 function handleBoxClick(event){
+    //если игра не запущена / закончилась
+    if (!isGameStarted) {
+        return //ф-я не выполняется
+    }
     //если элемент по клику содержит атрибут box
-   if(event.target.dataset.box) {  
+   if (event.target.dataset.box) {  
        score++ //увеличить счетчик элементов на 1
        renderBox() //сгенерировать квадрат
    }
@@ -43,6 +64,7 @@ function renderBox() {
     $game.insertAdjacentElement('afterbegin', box) //добавление элемента в dom-дерево
 }
 
+//ф-я для генерации рандомного числа
 function getRandom(min, max) {
     return Math.floor(Math.random() *(max - min) + min)
 }
